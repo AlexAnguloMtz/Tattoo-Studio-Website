a807fb92d5d2c250b06f10686c448d45110f76c1
Alex Angulo Martinez <raulalexo100@gmail.com>
2022-10-31 08:52:19 -0700

    Removed unnecessary h1 tag

--- src/components/FullScreenLayer/FullScreenLayer.tsx
+++ src/components/FullScreenLayer/FullScreenLayer.tsx
@@ -4,9 +4,7 @@ import './FullScreenLayer.scss';
 export const FullScreenLayer: FC<Props> = ({ children, className = '' }) => {
     return (
         <div className={`full-screen-layer ${className}`}>
-            <h1>
-                {children}
-            </h1>
+            {children}
         </div>
     );
 }
