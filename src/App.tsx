78e2d00a6e2f1b845f49b4c51b69c5e6031ec7b3
Alex Angulo Martinez <raulalexo100@gmail.com>
2022-11-04 16:59:06 -0700

    Switched from BrowserRouter to HashRouter (react-router gimmicks)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.scss';
 import { Nav } from './components/Nav/Nav';
-import { BrowserRouter, Routes } from "react-router-dom";
+import { HashRouter, Routes } from "react-router-dom";
 import { theLinks, routes } from './routing/routing';
 import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <HashRouter>
         <Nav links={theLinks} />
         <Routes>
           {routes()}
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
       {/*<FullScreenLayer className='landscape-only'>
         {<h1>La versión de escritorio sigue en construcción!
           Te invitamos a visitar la página en un móvil</h1>}
