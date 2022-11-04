eaa6f103abb97788cd372b9b8166246e7e9e9488
Alex Angulo Martinez <raulalexo100@gmail.com>
2022-11-03 19:02:59 -0700

    Fixed texts

--- src/pages/Home/BrandDescription/data.ts
+++ src/pages/Home/BrandDescription/data.ts
@@ -1,8 +1,9 @@
 export const data: string = `
     Somos un tattoo studio en Hermosillo, Sonora,
     comprometidos a brindarte un servicio de la más alta calidad.
-    Nuestros trabajos son una extensión de nosotros y nuestra identidad,
-    Puedes sentirte seguro de que saldras de nuestro estudio con una pieza de arte que vas a amar.
+    Nuestros trabajos son una extensión de nosotros y nuestra identidad.
+    Puedes sentirte seguro de que saldrás de nuestro 
+    estudio con una pieza de arte que vas a amar.
     Nuestra filosofía es que cada cliente,
     al igual que cada tatuaje, es único.
 `;
