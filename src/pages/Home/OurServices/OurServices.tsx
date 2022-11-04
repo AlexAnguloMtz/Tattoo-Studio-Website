b29dd660bf4f67a47b7f4706b1a8da9270c6e1bf
Alex Angulo Martinez <raulalexo100@gmail.com>
2022-11-03 18:42:29 -0700

    Fixed animation bug

--- src/pages/Home/OurServices/OurServices.tsx
+++ src/pages/Home/OurServices/OurServices.tsx
@@ -2,7 +2,6 @@ import { PageSection } from '../../../components/PageSection/PageSection';
 import { data } from './data';
 import imgMobile from '../../../pictures/Vikings/ship-small.jpg';
 import imgDesktop from '../../../pictures/tattoo-of-woman.jpg';
-import { useWindowSize } from 'usehooks-ts'
 import './OurServices.scss';
 import { RefObject, useRef } from 'react';
 import { useSimpleIntersectionObserver } from '../../../custom-hooks/useSimpleIntersectionObserver';
@@ -16,16 +15,19 @@ export const OurServices = () => {
 
     const isContentOnScreen = useSimpleIntersectionObserver({ ref, threshold: 0.3 });
 
-    const { width } = useWindowSize();
-
-    return (width < breakpoint) ? mobileLayout() : desktopLayout(ref, isContentOnScreen);
+    return (
+        <>
+            {mobileLayout()}
+            {desktopLayout(ref, isContentOnScreen)}
+        </>
+    );
 
 }
 
 const mobileLayout = () => {
     return (
         <PageSection
-            className='our-services__background'
+            className='our-services__background mobile'
             img={imgMobile}
             size={'full'}>
             {<p className='our-services__text'>{data}</p>}
@@ -35,7 +37,7 @@ const mobileLayout = () => {
 
 const desktopLayout = (ref: RefObject<HTMLDivElement>, isContentOnScreen: boolean) => {
     return (
-        <section ref={ref} className={`our-services ${isContentOnScreen ? ' on-screen' : ''}`}>
+        <section ref={ref} className={`our-services desktop ${isContentOnScreen ? ' on-screen' : ''}`}>
             <Overlay className='our-services__overlay'>
                 <img className='our-services__img' src={imgDesktop} alt="viking-ship" />
                 <p className='our-services__text'>{data}</p>
